3eb56dc89d693ab93dedd0328e1bd9d6413bb637
sync <50116007+synceye@users.noreply.github.com>
2021-04-17 22:57:17 +0200

    Update config.lua

--- esx_inventoryhud_glovebox/config.lua
+++ esx_inventoryhud_glovebox/config.lua
@@ -7,7 +7,7 @@ Config.AllowPolice = true -- If true, police will be able to search players' glo
 
 Config.Locale = "en"
 
-Config.OpenKey = 170
+Config.OpenKey = 182
 
 -- Limit, unit can be whatever you want. Originally grams (as average people can hold 25kg)
 Config.Weight = 1000
