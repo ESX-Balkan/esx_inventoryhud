a5d9ed2fb3466e5be2b000ee82a9a02496fa17a4
sync <50116007+synceye@users.noreply.github.com>
2021-04-17 22:54:33 +0200

    Update config.lua

--- esx_inventoryhud_trunk/config.lua
+++ esx_inventoryhud_trunk/config.lua
@@ -4,7 +4,7 @@ Config = {}
 Config.CheckOwnership = false -- If true, Only owner of vehicle can store items in trunk.
 Config.AllowPolice = true -- If true, police will be able to search players' trunks.
 
-Config.Locale = "nl"
+Config.Locale = "en"
 
 Config.OpenKey = 170
 
