364986c222acb4b1c19498b6710ff6dd8d92b9ba
sync <50116007+synceye@users.noreply.github.com>
2021-06-15 22:19:50 +0200

    Add files via upload

---
+++ esx_inventoryhud_glovebox/locales/inv_pretinac.lua
@@ -0,0 +1,13 @@
+Locales["hr"] = {
+  ["glovebox_closed"] = "Pretinac je zatvoren.",
+  ["no_veh_nearby"] = "Nema vozila u blizini.",
+  ["glovebox_in_use"] = "Netko vec drugi koristi ovaj glovebox.",
+  ["glovebox_full"] = "Pretinac je pun.",
+  ["invalid_quantity"] = "Kriva kolicina",
+  ["cant_carry_more"] = "Ne možeš nositi više",
+  ["nacho_veh"] = "Ovo nije tvoje vozilo.",
+  ["invalid_amount"] = "Kriva kolicina",
+  ["insufficient_space"] = "Nedostupna kolicina mjesta",
+  ["glovebox_info"] = "<h3>Pretinac</h3><br><strong>Registracija:</strong> %s<br><strong>Kolicina:</strong> %s / %s",
+  ["player_inv_no_space"] = "Nemaš dovoljno mjesta!"
+}
