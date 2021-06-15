b1b536712c0190ec1e11fc935e6937e58678eb0a
sync <50116007+synceye@users.noreply.github.com>
2021-06-15 22:20:28 +0200

    Add files via upload

---
+++ esx_inventoryhud_trunk/locales/inv_gepek.lua
@@ -0,0 +1,13 @@
+Locales["hr"] = {
+  ["trunk_closed"] = "Gepek je zatvoren.",
+  ["no_veh_nearby"] = "Nema vozila u blizini.",
+  ["trunk_in_use"] = "Netko vec drugi koristi ovaj trunk.",
+  ["trunk_full"] = "Gepek je pun.",
+  ["invalid_quantity"] = "Kriva kolicina",
+  ["cant_carry_more"] = "Ne možeš nositi više",
+  ["nacho_veh"] = "Ovo nije tvoje vozilo.",
+  ["invalid_amount"] = "Kriva kolicina",
+  ["insufficient_space"] = "Nedostupna kolicina mjesta",
+  ["trunk_info"] = "<h3>Gepek</h3><br><strong>Registracija:</strong> %s<br><strong>Kolicina:</strong> %s / %s",
+  ["player_inv_no_space"] = "Nemaš dovoljno mjesta!"
+}
